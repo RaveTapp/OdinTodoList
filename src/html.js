4dd436c7e6ce3c56f973531c36e48f64390b7f5a
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-07-01 13:35:34 +0200

    Begin making modal for add task

--- src/html.js
+++ src/html.js
@@ -46,7 +46,7 @@ function createProjectDiv(project, i){
     let plusIconElem = new Image();
     plusIconElem.src = plusIcon;
     plusIconElem.classList.add("plus");
-    plusIconElem.addEventListener("click", () => userCreateTask(i));
+    plusIconElem.addEventListener("click", () => modalPrompt(i));
     div.appendChild(plusIconElem);
     
 
@@ -59,3 +59,58 @@ document.querySelector("button.add-project").addEventListener("click", () => {
     createProject(name);
     projectsLoad();
 });
+
+
+//Add task
+export function modalPrompt(id){
+    console.log("prompt");
+    let modalDiv = document.createElement("div");
+    modalDiv.classList.add("modal");
+    let modalContent = document.createElement("div");
+    modalContent.classList.add("modal-content");
+    
+
+    let closeBtn = document.createElement("span");
+    closeBtn.classList.add("close");
+
+    let form = document.createElement("form");
+
+    let inputDiv1 = createInputDiv("Title of task:", "modal-title", "text", true);
+    let inputDiv2 = createInputDiv("Description:", "modal-desc", "text", false);
+    let inputDiv3 = createInputDiv("Due date:", "modal-date", "date", true);
+
+    let label4 = document.createElement("label");
+    label4.textContent = "Priority:";
+    label4.setAttribute("for", "modal-prio");
+    
+    
+    
+    let submit = document.createElement("input");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("value", "Add task");
+
+
+
+    modalContent.appendChild(closeBtn);
+    modalDiv.appendChild(modalContent);
+
+    content.appendChild(modalDiv);
+}
+
+function createInputDiv(labelText, id, inputType, isRequired){
+    let inputDiv = document.createElement("div");
+    
+    let label = document.createElement("label");
+    label.textContent = labelText;
+    label.setAttribute("for", id);
+
+    let input = document.createElement("input");
+    input.id = id;
+    input.setAttribute("type", inputType);
+    input.required = isRequired;
+
+    inputDiv.appendChild(label);
+    inputDiv.appendChild(input);
+       
+    return inputDiv;
+}
--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,8 @@
 import { projects, project, createProject} from "./todos.js";
+import { modalPrompt } from "./html.js";
 
 export function userCreateTask(id){
     console.log("Create" + id);
+    modalPrompt();
 }
+
